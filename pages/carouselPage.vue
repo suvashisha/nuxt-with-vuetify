11a944938afa5cb5c89e843747441505e5529d2d
suvashisha <suvashisha2011@gmail.com>
2022-05-05 10:14:20 +0900

    calender

---
+++ pages/carouselPage.vue
@@ -0,0 +1,140 @@
+<template>
+<v-container>
+  <div>{{ picker }}</div>
+  <v-carousel height="400" hide-delimiters>
+    <v-carousel-item v-for="(slide, i) in slides" :key="i">
+      <v-sheet :color="colors[i]" height="100%">
+        <v-row class="fill-height" align="center" justify="center">
+          <div class="main">
+            <v-date-picker
+              v-model="picker"
+              :first-day-of-week="1"
+              color="green lighten-1"
+              header-color="theme--dark"
+              locale="ja-jp"
+              :day-format="(date) => new Date(date).getDate()"
+              :month-format="
+                (timestamp) => new Date(timestamp.date).getMonth() + 1 + ' /'
+              "
+              :title-date-format="getTitleDateFormat"
+            ></v-date-picker>
+            <v-date-picker
+              v-model="picker"
+              :first-day-of-week="1"
+              color="green lighten-1"
+              header-color="theme--dark"
+              locale="ja-jp"
+              :day-format="(date) => new Date(date).getDate()"
+              :month-format="
+                (timestamp) => new Date(timestamp.date).getMonth() + 1 + ' /'
+              "
+              :title-date-format="getTitleDateFormat"
+            ></v-date-picker>
+            <v-date-picker
+              v-model="picker"
+              :first-day-of-week="1"
+              color="green lighten-1"
+              header-color="theme--dark"
+              locale="ja-jp"
+              :day-format="(date) => new Date(date).getDate()"
+              :month-format="
+                (timestamp) => new Date(timestamp.date).getMonth() + 1 + ' /'
+              "
+              :title-date-format="getTitleDateFormat"
+            ></v-date-picker>
+            <v-date-picker
+              v-model="picker"
+              :first-day-of-week="1"
+              color="green lighten-1"
+              header-color="theme--dark"
+              locale="ja-jp"
+              :day-format="(date) => new Date(date).getDate()"
+              :month-format="
+                (timestamp) => new Date(timestamp.date).getMonth() + 1 + ' /'
+              "
+              :title-date-format="getTitleDateFormat"
+            ></v-date-picker>
+          </div>
+        </v-row>
+      </v-sheet>
+    </v-carousel-item>
+  </v-carousel>
+  </v-container>
+</template>
+
+<script>
+export default {
+  name: "carouselPage",
+  data() {
+    return {
+      picker: new Date(Date.now() - new Date().getTimezoneOffset() * 60000)
+        .toISOString()
+        .substr(0, 10),
+      colors: [
+        "indigo",
+        "warning",
+        "pink darken-2",
+        "red lighten-1",
+        "deep-purple accent-4",
+      ],
+      slides: ["First", "Second", "Third", "Fourth", "Fifth"],
+    };
+  },
+  methods: {
+    getTitleDateFormat(isoDate) {
+      console.log("getTitleDateFormat: " + isoDate);
+      return new Date().toISOString().substr(5, 2);
+    },
+  },
+};
+</script>
+
+
+
+<style scoped>
+>>> .main {
+    overflow-x: scroll;
+    overflow-y: hidden;
+    white-space: nowrap;
+}
+>>> .v-window__next,
+>>> .v-window__prev {
+  top: 0%;
+  height: 400px;
+  border-top-left-radius: 0%;
+  border-top-right-radius: 0%;
+  border-bottom-right-radius: 0%;
+  border-bottom-left-radius: 0%;
+  margin: 0%;
+}
+>>> .v-window__next .v-btn,
+>>> .v-window__prev .v-btn {
+  top: calc(50% - 20px);
+}
+>>> .v-date-picker-header {
+  display: none;
+}
+>>> .v-date-picker-title__date {
+  font-size: 14px;
+}
+
+>>> .v-picker__title {
+  background: #424242;
+}
+>>> .v-date-picker-title {
+  width: 39px;
+  align-items: center;
+}
+>>> .v-picker__title {
+  padding: 2px 0 0 2px;
+}
+>>> .v-date-picker-table {
+  padding: 0px;
+}
+
+>>> .v-date-picker-title,
+>>> .v-date-picker-table--date th:nth-child(1),
+>>> .v-date-picker-table--date td:nth-child(1) {
+  background-color: lightgray;
+}
+</style>
---
+++ pages/dragScroll.vue
@@ -0,0 +1,120 @@
+<template>
+<div class="grid-container">
+  <main class="grid-item main">
+    <div class="items">
+      <div class="item item1"></div>
+      <div class="item item2"></div>
+      <div class="item item3"></div>
+      <div class="item item4"></div>
+      <div class="item item5"></div>
+      <div class="item item6"></div>
+      <div class="item item7"></div>
+      <div class="item item8"></div>
+      <div class="item item9"></div>
+      <div class="item item10"></div>
+    </div>
+  </main>
+</div>
+</template>
+
+<script>
+  export default {
+    name: 'dragScroll'
+
+  }
+</script>
+
+<style lang="scss" scoped>
+//VARIABLES BECAUSE CSS PREPROCESSORS ARE COOL
+$gray: #555;
+$yellow: #f2e968;
+$white: #efefef;
+
+
+.grid-container {
+  background: $white;
+  font-family: 'Rubik', sans-serif;
+}
+
+//ENABLE CSS GRID FOR LIFT OFF  🚀
+@supports(display: grid) {
+
+  .grid-container {
+    display: grid;
+    grid-template-columns: 1fr 1fr 1fr;
+    grid-template-rows: auto 1fr auto;
+
+    //Let the craziness begin!!!
+    grid-template-areas:
+      "header header header"
+      "title title footer"
+      "main main main";
+    @media screen and (max-width: 500px) {
+      grid-template-columns: 1fr;
+      grid-template-rows: 0.3fr 1fr auto 1fr;
+      grid-template-areas:
+        "header"
+        "title"
+        "main"
+    }
+  }
+
+  .grid-item {
+    color: #fff;
+    background: skyblue;
+    padding: 3.5em 1em;
+    font-size: 1em;
+    font-weight: 700;
+  }
+
+  .main {
+    color: lighten($gray, 25%);
+    background-color: lighten(skyblue, 60%);
+    grid-area: main;
+    padding: 0;
+    overflow-x: scroll;
+    overflow-y: hidden;
+  }
+
+  .items {
+    position: relative;
+    width: 100%;
+    overflow-x: scroll;
+    overflow-y: hidden;
+    white-space: nowrap;
+    transition: all 0.2s;
+    transform: scale(0.98);
+    will-change: transform;
+    user-select: none;
+    cursor: pointer;
+  }
+
+
+  .item {
+    display: inline-block;
+    background: skyblue;
+    min-height: 250px;
+    min-width: 400px;
+    margin: 2em 1em;
+    @media screen and (max-width: 500px) {
+      min-height: 200px;
+      min-width: 200px;
+    }
+  }
+}
+
+
+a {
+  display: block;
+  color: lighten(skyblue, 15%);
+  text-decoration: underline;
+  margin: 1em auto;
+  &:hover {
+    cursor: pointer;
+  }
+}
+
+i {
+  color: skyblue;
+}
+</style>
