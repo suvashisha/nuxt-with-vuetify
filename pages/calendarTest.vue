d06addf576140ededd35ebeabf9eb22a4e2e562e
suvashisha <suvashisha2011@gmail.com>
2022-05-08 23:18:44 +0900

    caleandarTest

---
+++ pages/calendarTest.vue
@@ -0,0 +1,162 @@
+<template>
+  <div>
+    <div>{{ this.$moment(today,'YYYY-MM-DD').format("YYYY/MM/DD") }}</div>
+    <v-sheet>
+      <v-slide-group multiple show-arrows>
+        <v-slide-item v-for="n in 6" :key="n">
+          <v-card class="fix_cal_wh">
+            <v-date-picker
+              v-model="$data['picker' + n]"
+              :first-day-of-week="0"
+              header-color="theme--dark"
+              color="cyan accent-2"
+              locale="ja-jp"
+              :day-format="(date) => new Date(date).getDate()"
+              :title-date-format="getTitleMonthFormat"
+              height="438px"
+              width="439px"
+            ></v-date-picker>
+          </v-card>
+        </v-slide-item>
+      </v-slide-group>
+    </v-sheet>
+      <div>
+    <v-divider></v-divider>
+  </div>
+  </div>
+</template>
+
+<script>
+export default {
+  name: "sample123",
+  data() {
+    return {
+      today: new Date().toISOString().substr(0, 10),
+      picker1: new Date(new Date().setMonth(new Date().getMonth() - 2))
+        .toISOString()
+        .substr(0, 10),
+      picker2: new Date(new Date().setMonth(new Date().getMonth() - 1))
+        .toISOString()
+        .substr(0, 10),
+      picker3: new Date().toISOString().substr(0, 10),
+      picker4: new Date(new Date().setMonth(new Date().getMonth() + 1))
+        .toISOString()
+        .substr(0, 10),
+      picker5: new Date(new Date().setMonth(new Date().getMonth() + 2))
+        .toISOString()
+        .substr(0, 10),
+      picker6: new Date(new Date().setMonth(new Date().getMonth() + 3))
+        .toISOString()
+        .substr(0, 10),
+    };
+  },
+  methods: {
+    getTitleMonthFormat(str) {
+      if (str === "") return str;
+      let m = str.substr(5, 2);
+      return m;
+    },
+    formatDate(date) {
+      if (!date) return null;
+      const [year, month, day] = date.split("-");
+      this.text = `${year}${month}${day}`;
+      return;
+    },
+  },
+};
+</script>
+
+<style scoped>
+/* >>> div.main_container {
+  width: 684px !important;
+} */
+
+>>> .v-btn::before {
+  background-color: transparent;
+}
+>>> .v-window__next,
+>>> .v-window__prev {
+  top: 0%;
+  height: 400px;
+  border-top-left-radius: 0%;
+  border-top-right-radius: 0%;
+  border-bottom-right-radius: 0%;
+  border-bottom-left-radius: 0%;
+  margin: 0%;
+}
+>>> .v-window__next .v-btn,
+>>> .v-window__prev .v-btn {
+  top: calc(50% - 20px);
+}
+>>> .v-date-picker-header {
+  display: none;
+}
+>>> .v-date-picker-title__date {
+  font-size: 14px;
+}
+
+/* >>> .v-picker__title {
+  background: #424242;
+} */
+>>> .v-date-picker-title {
+  width: 41.5px;
+  align-items: center;
+}
+
+>>> .v-date-picker-table {
+  /* padding: 0px; */
+  /* height: 226px; */
+  height: 356px;
+}
+
+/* >>> .v-date-picker-title,
+>>> .v-date-picker-table--date th:nth-child(1),
+>>> .v-date-picker-table--date td:nth-child(1) {
+  background-color: lightgray;
+} */
+
+>>> .v-date-picker-table table {
+  border-spacing: 0px !important;
+}
+>>> .v-date-picker-title {
+  pointer-events: none;
+}
+>>> .v-date-picker-title::before,
+>>> .v-picker__body::before {
+  height: 438px;
+  width: 70px;
+  content: " ";
+  position: absolute;
+  top: 0;
+  left: 0;
+  background-color: #1E1E1E;
+}
+>>> .v-picker__title,
+>>> .v-picker__body {
+  background-color: black;
+}
+>>> .v-picker__body {
+  border-radius: 0px !important;
+}
+.fix_cal_wh {
+  width: 439px;
+  height: 438px;
+}
+.v-picker {
+  font-size: 2.2em;
+}
+.v-divider {
+    border-color: #FFFFFF !important;
+    margin: 25px 25px 0 25px;
+}
+>>> .v-date-picker-table th,
+>>> .v-date-picker-table .v-btn {
+  font-size: 15px !important;
+}
+>>> .v-date-picker-title__year {
+  font-size: 16px !important;
+}
+>>> .v-date-picker-title__date {
+  font-size: 18px !important;
+}
+</style>
