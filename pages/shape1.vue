c8d75da0bd4bb2c141f8a9e7d11013bc37148581
suvashisha <suvashisha2011@gmail.com>
2022-05-30 00:10:08 +0900

    mod

--- pages/shape1.vue
+++ pages/shape1.vue
@@ -6,12 +6,45 @@
       <div class="line-vertical down arrow-down"></div>
       <div class="line-horizontal left arrow-left"></div>
       <div class="line-horizontal right arrow-right"></div>
+      <canvas id="canvas" class="not-blured-wrapper">
+        <!-- <div class="not-blured-circle"></div> -->
+      </canvas>
     </div>
   </div>
 </template>
 
 <script>
-export default {};
+export default {
+  data() {},
+  methods: {
+    createMetaball() {
+      let canvas = document.querySelector("#canvas");
+      let context = canvas.getContext("2d");
+
+      context.beginPath();
+      context.arc(50, 60, 40, 0, Math.PI * 2, true);
+      context.strokeStyle = "deepskyblue";
+      context.lineWidth = 5;
+      context.stroke();
+
+      context.beginPath();
+      context.arc(140, 60, 40, 0, Math.PI * 2, true);
+      context.fillStyle = "lightskyblue";
+      context.fill();
+
+      context.beginPath();
+      context.arc(230, 60, 40, 0, Math.PI * 2, true);
+      context.fillStyle = "lightskyblue";
+      context.fill();
+      context.strokeStyle = "deepskyblue";
+      context.lineWidth = 5;
+      context.stroke();
+    },
+  },
+  mounted() {
+    this.createMetaball();
+  },
+};
 </script>
 
 <style scoped>
@@ -51,7 +84,7 @@ export default {};
   margin-top: 105px;
 }
 .left {
-    margin-left: 10px;
+  margin-left: 10px;
 }
 .right {
   margin-left: 105px;
@@ -99,5 +132,26 @@ export default {};
 .arrow-right:before {
   right: -15px;
   border-left: 15px solid #aaa;
+}
+
+.not-blured-wrapper {
+  position: initial;
+  width: 160px;
+  height: 160px;
+  /* top: 20px;
+  left: 20px; */
+  margin: 20px;
+  border-radius: 20px;
+  background-color: #8000ff;
+}
+.not-blured-circle {
+  position: absolute;
+  border-radius: 100%;
+  background-color: #ff8000;
+  /* top: 45vh; */
+  /* left: 45vw; */
+  width: 5vmax;
+  height: 5vmax;
+  display: inline-block;
 }
 </style>
